079ebc1ae791892de6e778253e6a0aaf0c4b41fd
thomas779 <thomasehumphreys@gmail.com>
2023-10-13 12:55:43 -0500

    create user

--- src/v1/classes/User.ts
+++ src/v1/classes/User.ts
@@ -36,58 +36,33 @@ class User {
     return new Promise<User | undefined>(async (resolve, reject) => {
       const userTable = `user_421613_197`;
 
-      // Insert a row into the table
-      const { meta: insert } = await db
-        .prepare(
-          `INSERT INTO ${userTable} (user_id, username, registation_timestamp) VALUES (?, ?, ?);`
-        )
-        .bind(user.vault_id, user.username, user.registation_timestamp)
-        .run();
+      try {
+        // Insert a row into the table
+        const { meta: insert } = await db
+          .prepare(
+            `INSERT INTO ${userTable} (user_id, username, registation_timestamp) VALUES (?, ?, ?);`
+          )
+          .bind(user.vault_id, user.username, user.registation_timestamp)
+          .run();
 
-      // Wait for transaction finality
-      await insert.txn?.wait();
+        // Wait for transaction finality
+        const status = await insert.txn?.wait();
 
-      // Perform a read query, requesting all rows from the table
-      const { results } = await db.prepare(`SELECT * FROM ${userTable};`).all();
+        console.log(status, "status of waiting for tx");
 
-      console.log({ results }, "aaaa");
+        // Perform a read query, requesting all rows from the table
+        const { results } = await db
+          .prepare(`SELECT * FROM ${userTable};`)
+          .all();
 
-      // MySQL.pool.getConnection((err, db) => {
-      //   db.query(
-      //     "INSERT INTO `user` (username, registation_timestamp) VALUES (?,  UTC_TIMESTAMP());",
-      //     [user.username],
-      //     (err, results, fields) => {
-      //       if (err) {
-      //         reject(new ApiError(500, err.toString()));
-      //       } else {
-      //         // Check if results is an OkPacket (type guard)
-      //         if ("insertId" in results) {
-      //           const groupId = results.insertId;
-      //           // Create a query to select the newly inserted user
-      //           db.query(
-      //             "SELECT * FROM `user` WHERE vault_id = ?",
-      //             [groupId],
-      //             (err, results: RowDataPacket[], fields) => {
-      //               if (err) {
-      //                 reject(new ApiError(500, err.toString()));
-      //               } else {
-      //                 // Resolve with the selected user object
-      //                 const newUser = new User(results[0] as User);
-      //                 resolve(newUser);
-      //               }
-      //               db.release();
-      //             }
-      //           );
-      //         } else {
-      //           // Handle the case where results is not an OkPacket
-      //           reject(new ApiError(500, "Unexpected result format"));
-      //           db.release();
-      //         }
-      //       }
-      //       db.release();
-      //     }
-      //   );
-      // });
+        return { success: true };
+      } catch (e: any) {
+        return {
+          success: false,
+          message: e.message,
+          cause: e.cause.message,
+        };
+      }
     });
   };
 
